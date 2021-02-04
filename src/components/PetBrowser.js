693e6a75da8e50f8acc014eebd72e592bef5c955
Daniel <dyankiver@gmail.com>
2021-02-04 08:50:12 -0500

    Done.

--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -2,7 +2,12 @@ import React from "react";
 
 import Pet from "./Pet";
 
-function PetBrowser() {
+function PetBrowser({pets, onAdoptPet}) {
+
+  const petCards = pets.map((pet) => (
+    <Pet key={pet.id} pet={pet} onAdoptPet={onAdoptPet} />
+  ))
+
   return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>;
 }
 
